d1b471073909ec8a144b8e0222e00407ea0e98d7
ppyle <parkertpyle@gmail.com>
2022-07-07 17:33:16 -0600

    putting it together assignment

--- react/prop-it-up/src/components/PropItUp.jsx
+++ react/prop-it-up/src/components/PropItUp.jsx
@@ -1,12 +1,21 @@
 import React, {Component} from 'react';
 
 class PropItUp extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            personAge: this.props.personAge
+        }
+    }
+
     render(){
         return(
             <div className='person'>
                 <h2>{this.props.personName}</h2>
-                <p>age: {this.props.personAge}</p>
+                <p>age: {this.state.personAge}</p>
                 <p>Hair Color: {this.props.hairColor}</p>
+                <button onClick={() => this.setState({personAge: this.props.personAge+1})}>its your birthday {this.props.personName}!</button>
             
             </div>
         )
