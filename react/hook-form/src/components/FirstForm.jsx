657c7efec93946976fd587de443a517aa0fe4110
ppyle <parkertpyle@gmail.com>
2022-07-08 16:59:10 -0600

    added vaildations

--- react/hook-form/src/components/FirstForm.jsx
+++ react/hook-form/src/components/FirstForm.jsx
@@ -14,18 +14,42 @@ let [confirmpassword, setconfirmpassword] = useState("");
             <form action="#">
                 <label htmlFor="firstName"> First Name: </label>
             <input onChange={(e)=> setfirstName(e.target.value)} type="text" name="firstName"/> <br />
+            {
+                firstName.length < 3 ? <p>First name must be longer than 3 characters</p> :
+                null
+            }
 
             <label htmlFor="lastName">Last Name: </label>
             <input onChange={(e)=> setlastName(e.target.value)} type="text" name="lastName"/> <br />
 
+            {
+                lastName.length < 3 ? <p>last name must be longer than 3 characters</p> :
+                null
+            }
+
             <label htmlFor="email"> Email: </label>
             <input onChange={(e)=> setemail(e.target.value)} type="email" name="email"/>  <br />
+
+            {
+                email.length < 5 ? <p>email must be longer than 5 characters</p> :
+                null
+            }
 
             <label htmlFor="password"> Password: </label>
             <input onChange={(e)=> setpassword(e.target.value)} type="password" name="password"/> <br />
 
+            {
+                password.length < 8 ? <p>First name must be longer than 8 characters</p> :
+                null
+            }
+
             <label htmlFor="password"> Confirm Password: </label>
             <input onChange={(e)=> setconfirmpassword(e.target.value)} type="password" name="password"/> <br />
+
+            {
+                password != confirmpassword ? <p>Passwords must match to submit</p> :
+                null
+            }
 
             </form>
             
